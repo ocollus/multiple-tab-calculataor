5fbe8905d0296b4a809958bff6d387767671fff4
ocollus <bachson2002@gmail.com>
2024-09-20 17:18:22 +0700

    remove justify between

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Calc from './components/calc';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-8">
+    <main className="flex min-h-screen flex-col items-center p-8">
       <div className="z-10 max-w-[72rem] w-full items-center justify-between font-mono text-sm lg:flex">
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
           <a
